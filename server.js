2aab516bcab3cf7910c59719a6ce88219e7c648a
JoaoCzz <147420438+JoaoCzz@users.noreply.github.com>
2026-01-19 00:41:58 +0100

    dwdwd

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const itemsRouter = require('./routes/items');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// MIDDLEWARES NATIVOS (NO body-parser)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use('/api/items', itemsRouter);
