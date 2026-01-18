e70f8c037bc8330dce07d73b7d7ecbbda3a56b0a
JoaoCzz <147420438+JoaoCzz@users.noreply.github.com>
2026-01-19 00:55:01 +0100

    oo

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database.db');
 
-// Creo la Tabla 
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS items (
@@ -9,6 +8,12 @@ db.serialize(() => {
       nombre TEXT NOT NULL,
       descripcion TEXT
     )
+  `);
+
+  // INSERT FORZADO PARA DEMO
+  db.run(`
+    INSERT INTO items (nombre, descripcion)
+    VALUES ('Item demo', 'Creado al iniciar')
   `);
 });
 
